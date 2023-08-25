4a9c466067ca012214e48eda5d152eb038fd9c0a
Pomog <thoryur@gmail.com>
2023-08-25 21:39:20 +0300

    ascii-art-color START

--- main.go
+++ main.go
@@ -1,16 +1,49 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 
 	"github.com/Pomog/ascii-art/functions"
 )
 
 func main() {
 	args := os.Args[1:]
-	checkArgs(args)
+
+	// Define a flag named "color" with a default value and usage description
+	colorPtr := flag.String("color", "white", "Specify a color")
+
+	// Parse the command-line arguments
+	flag.Parse()
+
+	// Access the value of the "color" flag
+	color := *colorPtr
+
+	// Perform your logic based on the color
+	switch color {
+	case "red":
+		fmt.Println("You chose red!")
+	case "green":
+		fmt.Println("You chose green!")
+	case "blue":
+		fmt.Println("You chose blue!")
+	default:
+		fmt.Println("Unrecognized color:", color)
+	}
+
+	for i, arg := range args {
+		fmt.Printf("Argument %v: %s\n", i, arg)
+	}
+
+	checkArgs(args) // it shoud be 2 arguments: first - string, second - name of file with symbols
+
+	if len(args) == 4 {
+		// remove first 2 arguments
+		args = append(args[:0], args[2:]...)
+	}
 
 	unquotedString, errUnquot := strconv.Unquote((`"` + args[0] + `"`))
 	if errUnquot != nil {
@@ -44,8 +77,16 @@ func farewell(resultFileName string) {
 }
 
 func checkArgs(args []string) {
-	if len(args) != 2 {
+	argumentsCount := 2
+
+	if strings.Contains(args[0], "color") {
+		argumentsCount = 4
+	}
+
+	if len(args) != argumentsCount {
 		fmt.Println("Error: wrong number of arguments")
+		fmt.Println("Expected:", argumentsCount, "arguments")
+		fmt.Println("Got:", len(args), "arguments")
 		fmt.Println("Usage: go run main.go \"input string\" [BANNER]")
 		os.Exit(1)
 	}
