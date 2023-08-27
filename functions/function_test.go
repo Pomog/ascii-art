06087b1842d85121838530e503e71b7f3a83c8cc
Pomog <thoryur@gmail.com>
2023-08-27 12:15:38 +0300

    unit tests

---
+++ functions/function_test.go
@@ -0,0 +1,61 @@
+package functions
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func runTest(t *testing.T, testFunction interface{}, tests []struct {
+	input    interface{}
+	expected interface{}
+}) {
+	for _, test := range tests {
+		result := reflect.ValueOf(testFunction).Call([]reflect.Value{reflect.ValueOf(test.input)})[0].Interface()
+		fmt.Printf("Expected: %v, Got: %v\n", test.expected, result)
+		if !reflect.DeepEqual(result, test.expected) {
+			t.Errorf("For input %v, expected %v, got %v", test.input, test.expected, result)
+		}
+	}
+}
+
+func TestCheckForNotAllowedSymbols(t *testing.T) {
+	tests := []struct {
+		input    interface{}
+		expected interface{}
+	}{
+		{interface{}("Hello, World!\nWelcome to ASCII Art"), true},
+		{interface{}("Hello, World! ©"), false},
+	}
+	runTest(t, CheckForNotAllowedSymbols, tests)
+}
+
+func TestSplitStringByNewline(t *testing.T) {
+	tests := []struct {
+		input    interface{}
+		expected interface{}
+	}{
+		{"Hello, World!\nWelcome to ASCII Art", []string{"Hello, World!", "Welcome to ASCII Art"}},
+		{"Hello, World!", []string{"Hello, World!"}},
+		{"", []string{}},
+	}
+	for _, test := range tests {
+		result := splitStringByNewline(test.input.(string))
+		if !slicesMatch(result, test.expected.([]string)) {
+			t.Errorf("For input %v, expected %v, got %v", test.input, test.expected, result)
+		}
+	}
+}
+
+func slicesMatch(slice1, slice2 []string) bool {
+	if len(slice1) != len(slice2) {
+		return false
+	}
+
+	for i, element := range slice1 {
+		if element != slice2[i] {
+			return false
+		}
+	}
+	return true
+}
